3a5e1b97e9f3f6479c09f06b4b18766dd85c1f3a
Julio Okuda <julio.okuda@gmail.com>
2025-10-24 19:27:07 -0300

    Teste CI/CD final

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-  return{"mensagem":"Olá, deploy da api com Azure Web Service com CI/CD realizada com sucesso após ativar a implantação contínua!!!"}
+  return{"mensagem":"Deploy da api com Azure Web Service com CI/CD realizada com sucesso após ativar a implantação contínua!!!"}
